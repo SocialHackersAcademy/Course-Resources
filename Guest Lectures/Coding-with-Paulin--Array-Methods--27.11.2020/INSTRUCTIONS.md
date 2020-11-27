a0e969cd5bcb660be7227b1e301af0a024bb61fb
Kostas Minaidis <minaidisk@gmail.com>
2020-11-27 19:10:18 +0200

    Adding code from Guest Lecture 27.11.2020

---
+++ Guest Lectures/Coding-with-Paulin--Array-Methods--27.11.2020/INSTRUCTIONS.md
@@ -0,0 +1,23 @@
+# User management platform
+
+- Add a method to create a function that creates a user object with the following properties : first name, surname, age
+- Create an array with a few users on page startup
+- Add a method to return the user info as a string
+- Add a method to display all users in a list in our page
+- Add inputs and a button to add a user to our list
+- Add a surname and age inputs and take them into account when adding a new user
+- Fix the displayUsers function so it refresh the list content, not add content to it
+- Modify the display_users function to use forEach
+- Add a delete button next to each user listed, use slice to remove him from our array
+- Filter adults and minors using the filter method, and show them in two different part of the page.
+
+## To be fixed
+
+- make the displayUsers work when adding a user, create a refresh function with all the displayusers call
+- make the delete button work again using the filters
+
+## 27/11 tasks
+
+- Add a magic button to pass time, that will add one year to each users. Use the array method map to do that.
+- Create a function that gives the total age of the users using reduce
+- Sort the list of users by name, then first name, then age
---
+++ Guest Lectures/Coding-with-Paulin--Array-Methods--27.11.2020/src/user_management.js
@@ -0,0 +1,68 @@
+console.clear();
+
+function createUser(firstName, surname, age) {
+  return {
+    firstName: firstName,
+    surname: surname,
+    age: age
+  };
+}
+// INPUT: OBJECT -> OUTPUT: STRING
+function getUserInfo(user) {
+  //return user.firstName + " " + user.surname + ", " + user.age + " years old";
+  return `${user.firstName} ${user.surname}, ${user.age} years old`;
+}
+
+function displayUsers(listID, filtered_users) {
+  const user_list = document.getElementById(listID);
+    user_list.innerHTML = "";
+
+    filtered_users.forEach(function (user, index) {
+    const list_item = document.createElement("li");
+    const delete_button = document.createElement("button");
+    delete_button.addEventListener("click", function () {
+      users.splice(index, 1);
+      displayUsers();
+    });
+    delete_button.innerText = "Delete";
+    list_item.innerText = getUserInfo(user);
+    user_list.appendChild(list_item);
+    list_item.appendChild(delete_button);
+  });
+
+  
+
+  //for (let i = 0; i < users.length; i++){
+  /*
+  for (let user of users) {
+    const list_item = document.createElement("li");
+    list_item.innerText = getUserInfo(user); // or users[i]
+    user_list.appendChild(list_item);
+  } */
+}
+
+function addUser() {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const age = document.getElementById("age").value;
+  users.push(createUser(name, surname, age));
+  displayUsers();
+}
+
+const submitButton = document.getElementById("submit_button");
+submitButton.addEventListener("click", addUser);
+
+let users = [
+  createUser("John", "Doe", 21),
+  createUser("Kostas", "Athina", 12),
+  createUser("Simon", "Garfunkel", 35),
+  createUser("Scooby", "Doo", 17),
+  createUser("Ben", "Duwer", 66)
+];
+
+displayUsers('adult_list', users.filter(function(user) {
+  return user.age >= 18;
+}))
+displayUsers('youngsters_list', users.filter(function(user) {
+  return user.age < 18;
+}));
